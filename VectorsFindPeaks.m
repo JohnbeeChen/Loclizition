7fb98f23e5104640f88fead57083204819badb7d
Johnbee <Tianjiu14@outlook.com>
2017-05-03 09:18:02 +0800

    it seems that some problems occurred in drectly finding whole-heigh widths
    Signed-off-by: Johnbee <Tianjiu14@outlook.com>

--- VectorsFindPeaks.m
+++ VectorsFindPeaks.m
@@ -3,15 +3,42 @@ function varargout = VectorsFindPeaks(inVectors)
 % the row of the inVectors
 
 num = size(inVectors,1);
+% for ii = 1 : num
+%     [pks,locs,widths,proms] = findpeaks(inVectors(ii,:),'MinPeakProminence',1000);
+%     if ~isempty(pks) %it means that this curve have at leat an event
+%         event_num = size(pks,1);
+%         infos = [pks;locs;widths;proms];
+%         event_duration = zeros(event_num,3);
+%         % event start point estimator
+%         event_start = infos(2,:) - infos(3,:);
+%         % event end point estimator
+%         event_end = infos(2,:) + infos(3,:);
+%         for jj = 1 : event_num
+%             tem_start = event_start(jj);
+%             while 1
+%                 if tem_start <= 1
+%                    break;
+%                 else
+%                     
+%                 end
+%                 
+%             end
+%             tem_end = event_end(jj);
+%             while 1
+%                 
+%                 
+%             end
+%         end
+%     end
+% % end
+parfor ii = 1 : num
 
-for ii = 1 : num
-    
     event_iffos{ii} = My_FindPeaks(inVectors(ii,:));
-    %     [pks,locs,widths,proms] = findpeaks(inVectors(ii,:));
-    %     peaks_infos{ii}(1,:) = pks;
-    %     peaks_infos{ii}(2,:) = locs;
-    %     peaks_infos{ii}(3,:) = widths;
-    %     peaks_infos{ii}(4,:) = proms;
+%     [pks,locs,widths,proms] = findpeaks(inVectors(ii,:));
+%     peaks_infos{ii}(1,:) = pks;
+%     peaks_infos{ii}(2,:) = locs;
+%     peaks_infos{ii}(3,:) = widths;
+%     peaks_infos{ii}(4,:) = proms;
 end
 varargout{1} = event_iffos;
 
@@ -19,31 +46,32 @@ varargout{1} = event_iffos;
 function varargout = My_FindPeaks(inVector)
 % find a 1D signal's peaks and other information
 
-[pks,locs,widths,proms] = findpeaks(inVector);
+[pks,locs,widths,proms] = findpeaks(inVector,'MinPeakProminence',1000);
 pks_info = [pks; locs;widths;proms];
 % mean_proms = mean(proms(:));
-max_proms = max(proms(:));
-if max_proms < 200
-    
-end
-thre = max(proms(:))/2;
-id = pks_info(4,:) < 200;
-pks_info(:,id) = [];
-
-id = pks_info(4,:) < thre;
-pks_info(:,id) = [];
+% max_proms = max(proms(:));
+% if max_proms < 200
+%     
+% end
+% thre = max(proms(:))/2;
+% id = pks_info(4,:) < 200;
+% pks_info(:,id) = [];
+% 
+% id = pks_info(4,:) < thre;
+% pks_info(:,id) = [];
 num = size(pks_info,2);
 event_duration = zeros(num,3);
-if 1
+if 0
+    % 2 times half-heigh widths as the whole-high widths,it doesn't work well 
     % event start point
     event_duration(:,1) = pks_info(2,:) - pks_info(3,:);
     % event end point
-    event_duration(:,2) = pks_info(2,:) + pks_info(3,:); 
+    event_duration(:,2) = pks_info(2,:) + pks_info(3,:);
     event_duration(:,3) = 2 * pks_info(2,:) + 1;
     
-else 
+else % directly find the whole-high widths
     for ii = 1:num
-        tem = inVector - pks_info(1,ii)+ pks_info(4,ii) - 1;
+        tem = inVector - pks_info(1,ii)+ pks_info(4,ii) - 10;
         tem(tem<0) = 0;
         zeros_loc = find(~tem);
         
@@ -51,10 +79,7 @@ else
         end_loc = find(zeros_loc > pks_info(2,ii),1,'first');
         event_duration(ii,1:2) = [zeros_loc(satrt_loc),zeros_loc(end_loc)];
         event_duration(ii,3) =  event_duration(ii,2) -  event_duration(ii,1) + 1;
-        %     subplot(2,1,1),plot(inVector);
-        %     subplot(2,1,2),plot(tem);
-        %
-        %     nmb = 1;
+
     end
 end
 events_infos = mat2cell(pks_info,[1,1,1,1]);
--- test.m
+++ test.m
@@ -1,46 +1,7 @@
-
-% spmd
-%   A = rand(3,2); 
-% end
-% for i = 1:length(A)
-%    figure;imagesc(A{i}); 
-% end
-% tic
-% Data = 1:100;  
-% spmd  
-%     switch labindex  
-%         case 1  
-%             Data = Data+1;  
-%         case 2  
-%             Data = Data+2;  
-%     end  
-% end  
-% toc
-% parpool('local')
-% p  = gcp('nocreate');
-% if isempty(p)
-%     poolsize  = 0;
-% else
-%     poolsize = p.NumWorkers
-% end
-% delete(gcp('nocreate'))
-% parpool(2)
-% tic
-% spmd
-%    q = magic(labindex + 2); 
-% end
-% for ii  = 1 :length(q)
-%     figure;imagesc(q{ii});
-% end
-% toc
-x=0:2047;  
-a=load('data.txt');   %???data.txt?????????(current directory)?  
-n=5; % n???????????  
-mean = ones(1,n)./n;  %mean?1×n?????????????1/n  
-y = conv(a,mean);  
-y=y(1:length(y)-length(mean)+1);  
-figure;  
-subplot(1,2,1);plot(x,a);  
-xlabel('????????');  
-subplot(1,2,2);plot(x,y);  
-xlabel('????????');  
+A = swt_value;
+num = size(A,1);
+for ii = 1:num
+    
+   findpeaks(A(ii,:),'MinPeakProminence',1000,'Annotate','extents');
+   nmb
+end
